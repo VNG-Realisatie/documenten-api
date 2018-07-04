88d0a5b34674a2380a84d4445b456dcaa5c2efcf
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-04 12:58:24 +0200

    Build a new image if the commit is tagged in git

--- bin/release-docker-image.sh
+++ bin/release-docker-image.sh
@@ -5,7 +5,17 @@ set -x # echo commands
 
 CONTAINER_REPO=nlxio/gemma-drc
 
-RELEASE_TAG=${RELEASE_TAG:-latest}
+
+git_tag=$(git tag --points-at HEAD) &>/dev/null
+
+
+if [[ ! -z "$git_tag" ]]; then
+    echo "Building image for git tag $git_tag"
+    RELEASE_TAG=$git_tag
+else
+    RELEASE_TAG=${RELEASE_TAG:-latest}
+fi
+
 
 docker build \
     --target production \
