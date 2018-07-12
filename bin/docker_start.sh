cd1524d6835acaf93e6ff01cc1139a3ea1df5818
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-12 17:49:11 +0200

    Fix oopsie

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -25,4 +25,4 @@ python src/manage.py migrate
 
 # Start server
 >&2 echo "Starting server"
-uwsgi --http :8000 --module drc.wsgi --static-map /static=/app/static /media=/app/media  --chdir=src
+uwsgi --http :8000 --module drc.wsgi --static-map /static=/app/static --static-map /media=/app/media  --chdir=src
