02471d7e2f87e554548b98d249ff56e20fef1d62
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-17 09:22:23 +0200

    Ensure on Jenkins no DB volumes are needed
    This allows multiple builds of the same project concurrently since no
    containers hold/need state

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -3,6 +3,17 @@
 set -e
 set -x
 
+# Wait for the database container
+# See: https://docs.docker.com/compose/startup-order/
+db_host=${DB_HOST:-db}
+db_user=${DB_USER:-postgres}
+db_password=${DB_PASSWORD}
+
+until PGPASSWORD=$db_password psql -h "$db_host" -U "$db_user" -c '\q'; do
+  >&2 echo "Waiting for database connection..."
+  sleep 1
+done
+
 python src/manage.py jenkins \
   --noinput \
   --project-apps-tests \
