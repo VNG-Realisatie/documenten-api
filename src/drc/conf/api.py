49b09df8d6a448ebf0394c143e6965243e808480
Sergei Maertens <sergei@maykinmedia.nl>
2018-10-23 09:27:13 +0200

    Getting base64 in schema...
    However, now the error responses are messed up

--- src/drc/conf/api.py
+++ src/drc/conf/api.py
@@ -7,7 +7,9 @@ SWAGGER_SETTINGS = BASE_SWAGGER_SETTINGS.copy()
 SWAGGER_SETTINGS.update({
     'DEFAULT_INFO': 'drc.api.schema.info',
     # no geo things here
-    'DEFAULT_FIELD_INSPECTORS': BASE_SWAGGER_SETTINGS['DEFAULT_FIELD_INSPECTORS'][1:]
+    'DEFAULT_FIELD_INSPECTORS': (
+        'zds_schema.inspectors.files.FileFieldInspector',
+    ) + BASE_SWAGGER_SETTINGS['DEFAULT_FIELD_INSPECTORS'][1:]
 })
 
 GEMMA_URL_INFORMATIEMODEL_VERSIE = '1.0'
