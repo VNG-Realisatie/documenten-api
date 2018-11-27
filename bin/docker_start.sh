0ac0c0704524699a16a4dbf07c45dc9f9e90bfb1
Sergei Maertens <sergei@maykinmedia.nl>
2018-11-27 14:31:41 +0100

    :whale: Parametrize more in Docker
    The port that uwsgi should bind to can now be specified as well,
    allowing the network mode 'host' to be used in docker-compose

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-set -e  # exit on error
+set -ex
 
 # Wait for the database container
 # See: https://docs.docker.com/compose/startup-order/
-db_host=${DATABASE_HOST:-db}
-db_user=${DATABASE_USER:-postgres}
-db_password=${DATABASE_PASSWORD}
+db_host=${DB_HOST:-db}
+db_user=${DB_USER:-postgres}
+db_password=${DB_PASSWORD}
+db_port=${DB_PORT:-5432}
 
-until PGPASSWORD=$db_password psql -h "$db_host" -U "$db_user" -c '\q'; do
+uwsgi_port=${UWSGI_PORT:-8000}
+
+until PGPORT=$db_port PGPASSWORD=$db_password psql -h "$db_host" -U "$db_user" -c '\q'; do
   >&2 echo "Waiting for database connection..."
   sleep 1
 done
 
 >&2 echo "Database is up."
-
-# Collect static files
->&2 echo "Collect static files"
-python src/manage.py collectstatic --noinput
 
 # Apply database migrations
 >&2 echo "Apply database migrations"
@@ -26,12 +25,11 @@ python src/manage.py migrate
 # Start server
 >&2 echo "Starting server"
 uwsgi \
-    --http :8000 \
+    --http :$uwsgi_port \
     --module drc.wsgi \
     --static-map /static=/app/static \
     --static-map /media=/app/media  \
     --chdir src \
     --processes 2 \
     --threads 2
-    # processes & threads for concurrency, needed if there's no reverse proxy
-    # sitting in front and if there are 'circular' dependencies, which DRC has
+    # processes & threads are needed for concurrency without nginx sitting inbetween
