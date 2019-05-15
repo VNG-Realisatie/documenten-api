b7cffd948ded86b6d9f600b71c0588ede7129457
annashamray <anna.shamray@maykinmedia.nl>
2019-05-15 17:51:47 +0200

    clean imports

--- src/drc/urls.py
+++ src/drc/urls.py
@@ -5,7 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.views.generic.base import TemplateView
 
-import private_storage.urls
 
 handler500 = 'drc.utils.views.server_error'
 
@@ -17,12 +16,12 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('ref/', include('vng_api_common.urls')),
     path('ref/', include('vng_api_common.notifications.urls')),
-    path('private-media/', include(private_storage.urls)),
 ]
 
 # NOTE: The staticfiles_urlpatterns also discovers static files (ie. no need to run collectstatic). Both the static
 # folder and the media folder are only served via Django if DEBUG = True.
 urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.PRIVATE_MEDIA_URL, document_root=settings.PRIVATE_MEDIA_ROOT)
 
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
