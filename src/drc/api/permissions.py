a9be1213da2e1bbd8f9dff8339f0c626570c94dd
annashamray <anna.shamray@maykinmedia.nl>
2019-05-16 16:18:23 +0200

    remove unused function

--- src/drc/api/permissions.py
+++ src/drc/api/permissions.py
@@ -20,13 +20,3 @@ class InformationObjectRelatedAuthScopesRequired(RelatedObjAuthScopesRequired):
     """
     permission_fields = ('informatieobjecttype', 'vertrouwelijkheidaanduiding')
     obj_path = 'informatieobject'
-
-
-def allow_scopes(private_file) -> bool:
-    """
-    Check request to have a correct scope to retrieve media files
-    """
-    if private_file.request.jwt_auth.has_auth(scopes=SCOPE_DOCUMENTEN_ALLES_LEZEN):
-        return True
-
-    return False
