9dd39641e1d3e733388e21fdad8be926e3c7bec7
Steven Bal <steven@maykinmedia.nl>
2019-05-28 14:25:50 +0200

    Changed ZaakInformatieObjectValidator to ObjectInformatieObjectValidator to work with BRC

--- src/drc/api/serializers.py
+++ src/drc/api/serializers.py
@@ -20,8 +20,8 @@ from drc.datamodel.models import (
 
 from .auth import get_zrc_auth, get_ztc_auth
 from .validators import (
-    InformatieObjectUniqueValidator, StatusValidator,
-    ZaakInformatieObjectValidator
+    InformatieObjectUniqueValidator, ObjectInformatieObjectValidator,
+    StatusValidator
 )
 
 
@@ -186,7 +186,7 @@ class ObjectInformatieObjectSerializer(serializers.HyperlinkedModelSerializer):
                 'validators': [IsImmutableValidator()]
             }
         }
-        validators = [ZaakInformatieObjectValidator(), InformatieObjectUniqueValidator('object', 'informatieobject')]
+        validators = [ObjectInformatieObjectValidator(), InformatieObjectUniqueValidator('object', 'informatieobject')]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
