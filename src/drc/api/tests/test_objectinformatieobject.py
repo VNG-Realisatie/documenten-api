aa41406203291b4d89c449bace881cd011e4beb4
Sergei Maertens <sergei@maykinmedia.nl>
2019-06-13 16:00:54 +0200

    :white_check_mark: Add tests for OIO destroy

--- src/drc/api/tests/test_objectinformatieobject.py
+++ src/drc/api/tests/test_objectinformatieobject.py
@@ -1,9 +1,13 @@
+import uuid
+from unittest.mock import patch
+
 from django.test import override_settings
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 from vng_api_common.constants import ObjectTypes
 from vng_api_common.tests import JWTAuthMixin, get_validation_errors, reverse
+from zds_client.tests.mocks import mock_client
 
 from drc.datamodel.models import ObjectInformatieObject
 from drc.datamodel.tests.factories import (
@@ -53,17 +57,6 @@ class ObjectInformatieObjectTests(JWTAuthMixin, APITestCase):
         bio = eio.objectinformatieobject_set.get()
         self.assertEqual(bio.object, BESLUIT)
 
-    def test_delete(self):
-        oio = ObjectInformatieObjectFactory.create(
-            is_zaak=True
-        )
-        url = reverse(oio)
-
-        response = self.client.delete(url)
-
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(ObjectInformatieObject.objects.exists())
-
     def test_duplicate_object(self):
         """
         Test the (informatieobject, object) unique together validation.
@@ -100,8 +93,41 @@ class ObjectInformatieObjectTests(JWTAuthMixin, APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['informatieobject'], f'http://testserver{eo_detail_url}')
 
-    def test_destroy_oio_remote_gone(self):
-        raise NotImplementedError
 
-    def test_destroy_oio_remote_still_present(self):
-        raise NotImplementedError
+@patch('zds_client.client.get_operation_url')
+@patch('zds_client.tests.mocks.MockClient.fetch_schema', return_value={})
+class ObjectInformatieObjectDestroyTests(JWTAuthMixin, APITestCase):
+    heeft_alle_autorisaties = True
+
+    RESPONSES = {
+        "https://zrc.nl/api/v1/zaakinformatieobjecten": [],
+        "https://brc.nl/api/v1/besluitinformatieobjecten": [{
+            "url": f"https://brc.nl/api/v1/besluitinformatieobjecten/{uuid.uuid4()}",
+            "informatieobject": f"http://testserver/api/v1/enkelvoudiginformatieobjecten/{uuid.uuid4()}",
+            "besluit": BESLUIT,
+            "aardRelatieWeergave": "Legt vast, omgekeerd: is vastgelegd in",
+        }],
+    }
+
+    def test_destroy_oio_remote_gone(self, mock_fetch_schema, mock_get_operation_url):
+        mock_get_operation_url.return_value = '/api/v1/zaakinformatieobjecten'
+        oio = ObjectInformatieObjectFactory.create(is_zaak=True, object=ZAAK)
+        url = reverse(oio)
+
+        with mock_client(responses=self.RESPONSES):
+            response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(ObjectInformatieObject.objects.exists())
+
+    def test_destroy_oio_remote_still_present(self, mock_fetch_schema, mock_get_operation_url):
+        mock_get_operation_url.return_value = '/api/v1/besluitinformatieobjecten'
+        oio = ObjectInformatieObjectFactory.create(is_besluit=True, object=BESLUIT)
+        url = reverse(oio)
+
+        with mock_client(responses=self.RESPONSES):
+            response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        error = get_validation_errors(response, "nonFieldErrors")
+        self.assertEqual(error["code"], "remote-relation-exists")
