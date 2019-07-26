7f775580f4bb0cbddaf2eff4274ce921563f5eba
Sergei Maertens <sergei@maykinmedia.nl>
2019-07-26 14:47:29 +0200

    :pencil: Document webserver requirements

--- src/drc/api/schema.py
+++ src/drc/api/schema.py
@@ -1,15 +1,18 @@
 from django.conf import settings
 
+from humanize import naturalsize
 from drf_yasg import openapi
 
 from .kanalen import KANAAL_DOCUMENTEN
 
+min_upload_size = naturalsize(settings.MIN_UPLOAD_SIZE, binary=True)
+
 description = f"""Een API om een documentregistratiecomponent (DRC) te benaderen.
 
-In een documentregistratiecomponent worden INFORMATIEOBJECTen opgeslagen. Een 
-INFORMATIEOBJECT is een digitaal document voorzien van meta-gegevens. 
-INFORMATIEOBJECTen kunnen aan andere objecten zoals zaken en besluiten worden 
-gerelateerd (maar dat hoeft niet) en kunnen gebruiksrechten hebben. 
+In een documentregistratiecomponent worden INFORMATIEOBJECTen opgeslagen. Een
+INFORMATIEOBJECT is een digitaal document voorzien van meta-gegevens.
+INFORMATIEOBJECTen kunnen aan andere objecten zoals zaken en besluiten worden
+gerelateerd (maar dat hoeft niet) en kunnen gebruiksrechten hebben.
 
 GEBRUIKSRECHTEN leggen voorwaarden op aan het gebruik van het INFORMATIEOBJECT
 (buiten raadpleging). Deze GEBRUIKSRECHTEN worden niet door de API gevalideerd
@@ -17,6 +20,16 @@ of gehandhaafd.
 
 De typering van INFORMATIEOBJECTen is in de Catalogi API (ZTC) ondergebracht in
 de vorm van INFORMATIEOBJECTTYPEn.
+
+**Uploaden van bestanden**
+
+Binnen deze API bestaan een aantal endpoints die binaire data ontvangen, al
+dan niet base64-encoded. Webservers moeten op deze endpoints een minimale
+request body size van {min_upload_size} ondersteunen. Dit omvat de JSON van de
+metadata EN de base64-encoded bestandsdata. Hou hierbij rekening met de
+overhead van base64, die ongeveer 33% bedraagt in worst-case scenario's. Dit
+betekent dat bij een limiet van 4GB het bestand maximaal ongeveer 3GB groot
+mag zijn.
 
 **Afhankelijkheden**
 
