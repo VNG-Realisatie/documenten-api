f79b4521c6135da337d43f788afa9294da6b6f88
Steven Bal <steven@maykinmedia.nl>
2019-08-05 11:04:12 +0200

    Added notification documentation to API spec

---
+++ src/drc/api/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'drc.api.apps.DRCApiConfig'
---
+++ src/drc/api/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class DRCApiConfig(AppConfig):
+    name = 'drc.api'
+
+    def ready(self):
+        # ensure that the metaclass for every viewset has run
+        from . import viewsets  # noqa
