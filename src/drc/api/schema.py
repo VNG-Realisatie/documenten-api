768c532531728d3a4cad36bbee1e19ee7d3eb65e
Steven Bal <steven@maykinmedia.nl>
2020-09-14 16:14:10 +0200

    :art: Format with black

--- src/drc/api/schema.py
+++ src/drc/api/schema.py
@@ -67,7 +67,8 @@ info = openapi.Info(
     default_version=settings.API_VERSION,
     description=description,
     contact=openapi.Contact(
-        email="standaarden.ondersteuning@vng.nl", url=settings.DOCUMENTATION_URL,
+        email="standaarden.ondersteuning@vng.nl",
+        url=settings.DOCUMENTATION_URL,
     ),
     license=openapi.License(
         name="EUPL 1.2", url="https://opensource.org/licenses/EUPL-1.2"
