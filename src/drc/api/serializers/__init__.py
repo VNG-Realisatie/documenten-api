feba4ce27a8b843bd2333c04605aae12e4d87a53
Sonny Bakker <sonny@maykinmedia.nl>
2022-01-06 16:34:33 +0100

    Split serializers into separate files

---
+++ src/drc/api/serializers/__init__.py
@@ -0,0 +1,10 @@
+from .bestandsdeel import BestandsDeelSerializer
+from .enkelvoudig_informatieobject import (
+    EnkelvoudigInformatieObjectCreateLockSerializer,
+    EnkelvoudigInformatieObjectSerializer,
+    EnkelvoudigInformatieObjectWithLockSerializer,
+    LockEnkelvoudigInformatieObjectSerializer,
+    UnlockEnkelvoudigInformatieObjectSerializer,
+)
+from .gebruiksrechten import GebruiksrechtenSerializer
+from .object_informatieobject import ObjectInformatieObjectSerializer
