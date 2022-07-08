7f9740e39ff8e7c31914d095796fff1821eb41f5
matthijsbekendam <matthijsbekendam@gmail.com>
2022-07-08 11:51:38 +0200

    [#1770] removed exceptions.py

--- src/drc/api/exceptions.py
+++
@@ -1,9 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-from rest_framework.exceptions import APIException
-
-
-class OneAddressOnlyVerzendingException(APIException):
-    status_code = 400
-    default_detail = _(f"verzending must contain precisely one correspondentieadress")
-    default_code = "invalid-amount"
