b9127a70da3288afcef92cdc31064fcf361f2c16
matthijsbekendam <matthijsbekendam@gmail.com>
2022-10-25 14:19:36 +0200

    locked documents cannot be deleted

--- src/drc/api/tests/test_enkelvoudiginformatieobject.py
+++ src/drc/api/tests/test_enkelvoudiginformatieobject.py
@@ -471,10 +471,28 @@ class EnkelvoudigInformatieObjectVersionHistoryAPITests(JWTAuthMixin, APITestCas
         lock = self.client.post(f"{eio_url}/lock").data["lock"]
         self.client.patch(eio_url, {"beschrijving": "beschrijving2", "lock": lock})
 
+        url_unlock = get_operation_url(
+            "enkelvoudiginformatieobject_unlock", uuid=eio.uuid
+        )
+        unlock = self.client.post(url_unlock, {"lock": lock})
+
         response = self.client.delete(eio_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(EnkelvoudigInformatieObjectCanonical.objects.exists())
         self.assertFalse(EnkelvoudigInformatieObject.objects.exists())
+
+    def test_eio_delete_fails_on_locked(self):
+        eio = EnkelvoudigInformatieObjectFactory.create(beschrijving="beschrijving1")
+
+        eio_url = reverse(
+            "enkelvoudiginformatieobject-detail", kwargs={"uuid": eio.uuid}
+        )
+        lock = self.client.post(f"{eio_url}/lock").data["lock"]
+
+        response = self.client.delete(eio_url)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotEqual(lock, "")
 
     def test_eio_detail_retrieves_latest_version(self):
         eio = EnkelvoudigInformatieObjectFactory.create(beschrijving="beschrijving1")
