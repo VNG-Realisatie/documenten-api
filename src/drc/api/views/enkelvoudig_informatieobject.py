3db284d8b7e306511bbea94cd179f6015f1ddeea
matthijsbekendam <matthijsbekendam@gmail.com>
2023-02-14 14:11:38 +0100

    bad

--- src/drc/api/views/enkelvoudig_informatieobject.py
+++ src/drc/api/views/enkelvoudig_informatieobject.py
@@ -204,6 +204,8 @@ class EnkelvoudigInformatieObjectViewSet(
         description=_(
             "Zoeken/filteren gaat normaal via de `list` operatie, deze is echter niet geschikt voor zoekopdrachten met UUIDs."
         ),
+        request=YourRequestSerializer,
+        responses=YourResponseSerializer,
     )
     @action(methods=("post",), detail=False)
     def _zoek(self, request, *args, **kwargs):
