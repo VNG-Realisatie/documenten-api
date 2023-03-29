98c5e32db439fee3c9c7d9a23ea0e7b8b90f2ac4
matthijsbekendam <matthijsbekendam@gmail.com>
2023-03-29 18:30:05 +0200

    added: new versions changed: changelog, openapi.yaml

--- src/drc/__init__.py
+++ src/drc/__init__.py
@@ -7,7 +7,7 @@
 import re
 from collections import namedtuple
 
-__version__ = "1.2.3"
+__version__ = "1.3.0"
 __author__ = "VNG Realisatie"
 __homepage__ = "https://github.com/VNG-Realisatie/documenten-api"
 __docformat__ = "restructuredtext"
