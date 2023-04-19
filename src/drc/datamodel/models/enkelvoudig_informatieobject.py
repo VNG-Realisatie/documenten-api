ea261c833d69ed1ec95aa79f90cb1639de4a40db
matthijsbekendam <matthijsbekendam@gmail.com>
2023-04-19 14:49:16 +0200

    added trefwoorden filter

--- src/drc/datamodel/models/enkelvoudig_informatieobject.py
+++ src/drc/datamodel/models/enkelvoudig_informatieobject.py
@@ -8,6 +8,7 @@ from privates.fields import PrivateMediaFileField
 from vng_api_common.caching import ETagMixin
 from vng_api_common.descriptors import GegevensGroepType
 from vng_api_common.models import APIMixin
+from django.contrib.postgres.fields import ArrayField
 
 from ..constants import ChecksumAlgoritmes
 from .informatieobject import InformatieObject
@@ -68,15 +69,15 @@ class EnkelvoudigInformatieObject(ETagMixin, APIMixin, InformatieObject):
         max_length=255,
         blank=True,
         help_text='Het "Media Type" (voorheen "MIME type") voor de wijze waarop'
-        "de inhoud van het INFORMATIEOBJECT is vastgelegd in een "
-        "computerbestand. Voorbeeld: `application/msword`. Zie: "
-        "https://www.iana.org/assignments/media-types/media-types.xhtml",
+                  "de inhoud van het INFORMATIEOBJECT is vastgelegd in een "
+                  "computerbestand. Voorbeeld: `application/msword`. Zie: "
+                  "https://www.iana.org/assignments/media-types/media-types.xhtml",
     )
     taal = models.CharField(
         max_length=3,
         help_text="Een ISO 639-2/B taalcode waarin de inhoud van het "
-        "INFORMATIEOBJECT is vastgelegd. Voorbeeld: `dut`. Zie: "
-        "https://www.iso.org/standard/4767.html",
+                  "INFORMATIEOBJECT is vastgelegd. Voorbeeld: `dut`. Zie: "
+                  "https://www.iso.org/standard/4767.html",
     )
 
     bestandsnaam = models.CharField(
@@ -101,7 +102,7 @@ class EnkelvoudigInformatieObject(ETagMixin, APIMixin, InformatieObject):
         max_length=200,
         blank=True,
         help_text="De URL waarmee de inhoud van het INFORMATIEOBJECT op te "
-        "vragen is.",
+                  "vragen is.",
     )
 
     # these fields should not be modified directly, but go through the `integriteit` descriptor
@@ -147,6 +148,8 @@ class EnkelvoudigInformatieObject(ETagMixin, APIMixin, InformatieObject):
             "gewijzigd."
         ),
     )
+
+    trefwoorden = ArrayField(models.CharField(max_length=100), help_text="Een lijst van trefwoorden gescheiden door comma's.", blank=True, null=True)
 
     class Meta:
         unique_together = ("uuid", "versie")
