fe56207c68256c92486931587fd0d2afd94f9a83
matthijsbekendam <matthijsbekendam@gmail.com>
2023-08-29 13:39:39 +0200

    update: lock write_only and bumpversion to 1.2.4.

--- src/drc/__init__.py
+++ src/drc/__init__.py
@@ -7,7 +7,7 @@
 import re
 from collections import namedtuple
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 __author__ = "VNG Realisatie"
 __homepage__ = "https://github.com/VNG-Realisatie/documenten-api"
 __docformat__ = "restructuredtext"
